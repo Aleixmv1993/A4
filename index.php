0e4266d839e9c37b1f9ae74be7397ef8dee0a0e9
Aleixmv1993 <aleixmalaretacad@gmail.com>
2018-02-15 16:02:14 +0100

    Copia seg

--- index.php
+++ index.php
@@ -1,4 +1,30 @@
 <?php
 
-    echo "Funciona";
+    require 'sys/DB.php';
+    
+    use App\sys\DB;
+    
+    
+    define('CONF',__DIR__.'/Config.json');
+    
+    $gdb = DB::getInstance();
+      
+
+    $sql="SELECT * FROM users WHERE username = :username";
+
+    $gdb->query($sql); 
+    $gdb->bind(':username', "AleixAdmin");
+    $res = $gdb->execute();
+
+    
+    if($res == 1){
+        $result = $gdb->single();
+        echo $result[username];
+    }
+    
+    $sql2 ="SELECT username FROM users WHERE rol = :rol";
+    
+    $gdb->query($sql); 
+    $gdb->bind(':rol', 1);
+    $res = $gdb->execute();
 
---
+++ sys/DB.php
@@ -0,0 +1,94 @@
+<?php
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace App\Sys;
+
+require 'Helper.php';
+use App\Sys\Helper;
+
+/**
+ * Description of DB
+ *
+ * @author linux
+ */
+class DB extends \PDO{
+    
+    private $stmt;
+    static private $_instance=null;
+    
+    
+    static function getInstance(){
+        if(!(self::$_instance instanceof self)){
+            self::$_instance=new self();
+        }
+        
+        return self::$_instance;
+        
+    }
+    function __construct(){
+       
+        $dbconf=Helper::getConfig();
+        
+        $dsn=$dbconf['driver'].':host='.$dbconf['dbhost'].';dbname='.$dbconf['dbname'];
+        $usr=$dbconf['dbuser'];
+        $pwd=$dbconf['dbpass'];
+        
+        try{
+        parent::__construct($dsn,$usr,$pwd);
+        
+        } catch (PDOException $e){
+            echo $e->getMessage();
+        }
+        
+    }
+    
+    public function query($sql){
+        try{
+        $this->stmt = self::$_instance->prepare($sql);
+        } catch (\PDOException $ex){
+            echo $ex->getMessage();
+        }
+    }
+    
+    public function bind($param,$value){
+        
+        $type = null;
+        
+        switch (true){
+        case is_bool($value):
+            $type = \PDO::PARAM_BOOL;
+        break;
+        case is_string($value):
+            $type = \PDO::PARAM_STR;
+        break;
+        case is_int($value):
+            $type = \PDO::PARAM_INT;
+        break;
+        default:
+            $type = \PDO::PARAM_NULL;
+        break;
+        
+        }
+        $this->stmt->bindValue($param, $value, $type);
+    }
+    
+    public function execute(){
+        
+      return $this->stmt->execute();
+        //return $this->stmt->execute();
+        
+    }
+    
+    public function resultSet(){
+        return $this->stmt->fetchAll(\PDO::FETCH_ASSOC);
+    }
+    public function single(){
+        return $this->stmt->fetch(\PDO::FETCH_ASSOC);              
+    }
+            
+                
+}
